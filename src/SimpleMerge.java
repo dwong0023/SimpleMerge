219b531e2a385aa334065daba44a1c5283c78aa1
dwong0023 <dwong0023@bths.edu>
2019-03-07 17:27:55 -0500

    simplemergedone

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -7,9 +7,17 @@
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
     int[] finalarray = new int[arr1.length+arr2.length];
-    for(int i = 0; i < finalarray.length;i++)
+    int[] firstarray = new int[arr1.length];
+    int[] secarray = new int[arr1.length];
+    for(int i = 0; i < arr1.length;i++)
     {
-        finalarray[i] = Math.min(arr1[i],arr2[i]);
+        firstarray[i] = Math.min(arr1[i],arr2[i]);
+        secarray[i] = Math.max(arr1[i],arr2[i]);
+    }
+    for(int i = 0; i < arr1.length;i++)
+    {
+        finalarray[i] = firstarray[i];
+        finalarray[i+arr1.length] = secarray[i];
     }
     return finalarray;
     }
--- src/SimpleMergeRunner.java
+++ src/SimpleMergeRunner.java
@@ -2,13 +2,14 @@
  * Created by Teacher on 1/28/2019.
  */
 public class SimpleMergeRunner {
+    public static int[] resultArr= new int[10];
     public static void main(String[] args)
     {
         int[] arr1 = {1,3,5,7,9};
         int[] arr2 = {2,4,6,8,10};
         System.out.println("Merging: {1,3,5,7,9} and {2,4,6,8,10}");
-        int[] resultArr = SimpleMerge.simpleMerge(arr1,arr2);
-        for (int i:resultArr)
+        resultArr = SimpleMerge.simpleMerge(arr1,arr2);
+        for (int i = 0; i < resultArr.length-1;i++)
             System.out.print(i+" ");
         System.out.println();
 
@@ -23,7 +24,7 @@ public class SimpleMergeRunner {
         int[] arr5 = {1,2,3,9,10};
         int[] arr6 = {4,5,6,7,8};
         System.out.println("Merging: {1,2,3,9,10} and {4,5,6,7,8}");
-        resultArr = SimpleMerge.simpleMerge(arr3,arr4);
+        resultArr = SimpleMerge.simpleMerge(arr5,arr6);
         for (int i:resultArr)
             System.out.print(i+" ");
         System.out.println();
